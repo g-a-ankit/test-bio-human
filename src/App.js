af363d35ce24119db56d5467618d11645bceaebf
g-a-ankit <gaankit99@gmail.com>
2022-04-07 12:21:45 +0530

    feat: use api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Helmet from "react-helmet";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Helmet></Helmet>
+      <iframe
+        id="myWidget"
+        src="https://human.biodigital.com/widget/?m=cochlear_implant&dk=388e873b30a77e3f6d78ea71ed0e89a6a02a7a43"
+        width="1500vh"
+        height="1000vh"
+      ></iframe>
+      <script src="https://developer.biodigital.com/builds/api/2/human-api.min.js"></script>
+    </>
   );
 }
 
