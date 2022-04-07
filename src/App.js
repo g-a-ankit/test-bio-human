ebfa68981691f14dc89e3f5fb7054bef1a69e596
g-a-ankit <gaankit99@gmail.com>
2022-04-07 12:37:15 +0530

    feat: resize screen

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ function App() {
   return (
     <>
       <Helmet></Helmet>
-      <iframe
-        id="myWidget"
-        src="https://human.biodigital.com/widget/?m=cochlear_implant&dk=388e873b30a77e3f6d78ea71ed0e89a6a02a7a43"
-        width="1500vh"
-        height="1000vh"
-      ></iframe>
-      <script src="https://developer.biodigital.com/builds/api/2/human-api.min.js"></script>
+      <div style={{ width: "100%", height: "100vh" }}>
+        <iframe
+          id="myWidget"
+          src="https://human.biodigital.com/widget/?m=cochlear_implant&dk=388e873b30a77e3f6d78ea71ed0e89a6a02a7a43"
+          width="100%"
+          height="100%"
+        ></iframe>
+        <script src="https://developer.biodigital.com/builds/api/2/human-api.min.js"></script>
+      </div>
     </>
   );
 }
